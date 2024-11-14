7531a0715455b7701fad7be2edd32bea52c8cf93
Howon Jung <jhwlds@gmail.com>
2024-11-14 13:37:40 -0700

    working on 3rd party api 2

--- src/3rd party/motivatioin.jsx
+++ src/3rd party/motivatioin.jsx
@@ -1,3 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export function Motivation() {
+    const [phrases, setPhrases] = useState([]);
+
+
+    return (
+
+    );
+}
