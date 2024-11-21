5ef913b37d70e009460ebe33f7b950064d561c7c
Howon Jung <jhwlds@gmail.com>
2024-11-21 16:17:51 -0700

    working on database

--- service/database.js
+++ service/database.js
@@ -9,42 +9,59 @@ const db = client.db('Startup');
 const userCollection = db.collection('user');
 const scoreCollection = db.collection('score');
 
-async function connectDB() {
-    if (!client.isConnected()) {
-        await client.connect();
-    }
+
+(async function testConnection() {
+  try {
+    await client.connect();
+    await db.command({ ping: 1 });
+    console.log('Database connection test successful!');
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})();
+
+
+async function createUser(username, password) {
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const userId = uuid.v4();
+
+  const user = {
+    userId,
+    username,
+    password: hashedPassword,
+  };
+
+  await userCollection.insertOne(user);
+  return { userId, username };
 }
 
+
+async function loginUser(username, password) {
+  const user = await userCollection.findOne({ username });
+  if (user && await bcrypt.compare(password, user.password)) {
+    return { userId: user.userId, username: user.username };
+  }
+  return null;
+}
+
+
+async function submitScore(username, score) {
+  const timestamp = new Date().toISOString();
+  await scoreCollection.insertOne({ username, score, timestamp });
+
+  const topScores = await scoreCollection
+    .find({})
+    .sort({ score: -1, timestamp: 1 })
+    .limit(5)
+    .toArray();
+  return topScores;
+}
+
+
 module.exports = {
-    connectDB,
-
-    async createUser(username, password) {
-        await connectDB();
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const userId = uuid.v4();
-        await userCollection.insertOne({ userId, username, password: hashedPassword });
-        return { userId, username };
-    },
-
-    async loginUser(username, password) {
-        await connectDB();
-        const user = await userCollection.findOne({ username });
-        if (user && await bcrypt.compare(password, user.password)) {
-            return { userId: user.userId, username: user.username };
-        }
-        return null;
-    },
-
-    async submitScore(username, score) {
-        await connectDB();
-        const timestamp = new Date().toISOString();
-        await scoreCollection.insertOne({ username, score, timestamp });
-
-        const topScores = await scoreCollection
-            .find({})
-            .sort({ score: -1, timestamp: 1 })
-            .limit(5)
-            .toArray();
-        return topScores;
-    },
+  createUser,
+  loginUser,
+  submitScore,
+  getHighScores,
 };
